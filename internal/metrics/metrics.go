77db9fa0857dfe40d071035c5cea9cae91ccc48e
Nick Dombroski <ndombroski@digitalocean.com>
2022-05-05 09:53:54 -0400

    Add custom prometheus metrics
    -Create package metrics used to register custom metrics to the
    kubernetes controller manager
    -Add a netbox_requests_total metric

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,24 @@
+// package metrics contains all custom metrics to be exported to prometheus
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	kubemetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+// Init registers all internal metrics to the given prometheus registry
+func Init(r *kubemetrics.RegistererGatherer) {
+	(*r).MustRegister(netboxTotalRequests)
+}
+
+var (
+	netboxTotalRequests = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "netbox_requests_total",
+		Help: "Total number of requests sent to the NetBox API server",
+	})
+)
+
+// IncremementNetboxTotalRequests increments the netbox_total_requests metric
+func IncrementNetboxTotalRequests() {
+	netboxTotalRequests.Inc()
+}
