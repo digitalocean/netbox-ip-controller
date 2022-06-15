446ed9fad577fb3fa82f012b1d1f25546faac53c
Nick Dombroski <ndombroski@digitalocean.com>
2022-06-15 15:53:37 -0400

    Enforce comment format on exported func/type/vars
    Enable checks from staticcheck that enforce comments on exported
    functions, types, and variables to start with the name. Fix existing
    issues that violate this rule.

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -119,7 +119,7 @@ func WithClusterDomain(domain string) Option {
 	}
 }
 
-// If passed true, WithDualStackIP enables registering both IPv6 and IPv4 address in netbox
+// WithDualStackIP enables registering both IPv6 and IPv4 address in netbox
 // for dual stack pods and services.
 func WithDualStackIP() Option {
 	return func(s *Settings) error {
